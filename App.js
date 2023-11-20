38817bff53d758a450d27e235d1eca9c0c8570ab
Nehapatil9096 <141837328+Nehapatil9096@users.noreply.github.com>
2023-11-20 17:45:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import Rectangle from './Rectangle';
+
+const App = () => {
+  return (
+    <div>
+      <Rectangle />
+    </div>
+  );
+};
+
+export default App;
